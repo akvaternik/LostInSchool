6728579cdc178bca130ad96f2c584997d6d9b1f5
akvaternik <adrien.kvaternik@centrale-marseille.fr>
2014-02-15 18:29:54 +0100

    login with database

--- src/main/java/web/controller/LoginController.java
+++ src/main/java/web/controller/LoginController.java
@@ -1,21 +1,36 @@
 package web.controller;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
+import java.net.UnknownHostException;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login/{user}/{pwd}")
     @ResponseBody
-    public String login(@PathVariable String user, @PathVariable String pwd){
+    public String login(@PathVariable String user, @PathVariable String pwd) throws UnknownHostException {
         String access = "";
-        String userID = "Adrien";
-        String password = "coucou";
+
+        DB db = new MongoClient().getDB("LostInSchool");
+        Jongo jongo = new Jongo(db);
+        MongoCollection users = jongo.getCollection("users");
+        JSONObject jsonObject = users.findOne("{userID: " + "'" + user + "'}").projection("{_id:0}").as(JSONObject.class);
+        if(jsonObject == null){
+            return access;
+        }
+
+        String userID = (String) jsonObject.get("userID");
+        String password = (String) jsonObject.get("password");
 
         if(userID.equals(user) && password.equals(pwd)){
             access = "ok";
